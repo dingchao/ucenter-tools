d7d66e60fbbd11f895e4f95a489e62d10ba87480
bicliu <klins@163.com>
2018-04-03 14:01:26 +0800

    1

--- walletd/walletd.cpp
+++ walletd/walletd.cpp
@@ -48,55 +48,6 @@ public:
         // Testnet Ulord BIP44 coin type is '1' (All coin's testnet default)
         base58Prefixes[EXT_COIN_TYPE]  = boost::assign::list_of(0x80)(0x00)(0x00)(0x01).convert_to_container<std::vector<unsigned char> >();
     };
-
-    std::string EncodeBase58(const unsigned char* pbegin, const unsigned char* pend)
-    {
-        // Skip & count leading zeroes.
-        int zeroes = 0;
-        while (pbegin != pend && *pbegin == 0) {
-            pbegin++;
-            zeroes++;
-        }
-        // Allocate enough space in big-endian base58 representation.
-        std::vector<unsigned char> b58((pend - pbegin) * 138 / 100 + 1); // log(256) / log(58), rounded up.
-        // Process the bytes.
-        while (pbegin != pend) {
-            int carry = *pbegin;
-            // Apply "b58 = b58 * 256 + ch".
-            for (std::vector<unsigned char>::reverse_iterator it = b58.rbegin(); it != b58.rend(); it++) {
-                carry += 256 * (*it);
-                *it = carry % 58;
-                carry /= 58;
-            }
-            assert(carry == 0);
-            pbegin++;
-        }
-        // Skip leading zeroes in base58 result.
-        std::vector<unsigned char>::iterator it = b58.begin();
-        while (it != b58.end() && *it == 0)
-            it++;
-        // Translate the result into a string.
-        std::string str;
-        str.reserve(zeroes + (b58.end() - it));
-        str.assign(zeroes, '1');
-        while (it != b58.end())
-            str += pszBase58[*(it++)];
-        return str;
-    };
-
-    std::string EncodeBase58(const std::vector<unsigned char>& vch)
-    {
-        return EncodeBase58(&vch[0], &vch[0] + vch.size());
-    };
-
-    std::string EncodeBase58Check(const std::vector<unsigned char>& vchIn)
-    {
-        // add 4-byte hash check to the end
-        std::vector<unsigned char> vch(vchIn);
-        uint256 hash = Hash(vch.begin(), vch.end());
-        vch.insert(vch.end(), (unsigned char*)&hash, (unsigned char*)&hash + 4);
-        return EncodeBase58(vch);
-    };
 }
 
 void printhelp()
